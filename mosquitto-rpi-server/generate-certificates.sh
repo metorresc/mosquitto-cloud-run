ca09112585e9903468f2b0c5272b6a4fcf78ec02
Manuel Torres <manuel@axmos.tech>
2022-11-09 16:52:33 -0300

    Toma Tusssi

--- mosquitto-rpi-server/generate-certificates.sh
+++ mosquitto-rpi-server/generate-certificates.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 #(@)generate-CA.sh - Create CA key-pair and server key-pair signed by CA
 
-# Copyright (c) 2013-2016 Jan-Piet Mens <jpmens()gmail.com>
+# Copyright (c) 2013-2020 Jan-Piet Mens <jpmens()gmail.com>
 # All rights reserved.
-# https://github.com/owntracks/tools/blob/master/TLS/generate-CA.sh
-#
+# 
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-#
+# 
 # 1. Redistributions of source code must retain the above copyright notice,
 #    this list of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright
@@ -16,7 +15,7 @@
 # 3. Neither the name of mosquitto nor the names of its
 #    contributors may be used to endorse or promote products derived from
 #    this software without specific prior written permission.
-#
+# 
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -31,16 +30,16 @@
 
 #
 # Usage:
-#       ./generate-CA.sh                creates ca.crt and server.{key,crt}
-#       ./generate-CA.sh hostname       creates hostname.{key,crt}
-#       ./generate-CA.sh client email   creates email.{key,crt}
+#	./generate-CA.sh		creates ca.crt and server.{key,crt}
+#	./generate-CA.sh hostname	creates hostname.{key,crt}
+#	./generate-CA.sh client email	creates email.{key,crt}
 #
 # Set the following optional environment variables before invocation
 # to add the specified IP addresses and/or hostnames to the subjAltName list
 # These contain white-space-separated values
 #
-#       IPLIST="172.13.14.15 192.168.1.1"
-#       HOSTLIST="a.example.com b.example.com"
+#	IPLIST="172.13.14.15 192.168.1.1"
+#	HOSTLIST="a.example.com b.example.com"
 
 set -e
 
@@ -49,14 +48,14 @@ export LANG=C
 kind=server
 
 if [ $# -ne 2 ]; then
-        kind=server
-        host=$(hostname -f)
-        if [ -n "$1" ]; then
-                host="$1"
-        fi
+	kind=server
+	host=$(hostname -f)
+	if [ -n "$1" ]; then
+		host="$1"
+	fi
 else
-        kind=client
-        CLIENT="$2"
+	kind=client
+	CLIENT="$2"
 fi
 
 [ -z "$USER" ] && USER=root
@@ -66,8 +65,8 @@ DIR=${TARGET:='.'}
 # may be empty ""
 ALTHOSTNAMES=${HOSTLIST}
 ALTADDRESSES=${IPLIST}
-CA_ORG='/O=IoT/OU=MQTT/emailAddress=mqtt-service@axmos.tech'
-CA_DN="/CN=MQTT broker${CA_ORG}"
+CA_ORG='/O=OwnTracks.org/OU=generate-CA/emailAddress=root@axmos.tech'
+CA_DN="/CN=Edge MQTT broker${CA_ORG}"
 CACERT=${DIR}/ca
 SERVER="${DIR}/${host}"
 SERVER_DN="/CN=${host}$CA_ORG"
@@ -79,211 +78,217 @@ MOSQUITTOUSER=${MOSQUITTOUSER:=$USER}
 # version of OpenSSL, run `openssl dgst -help` and set your
 # signature algorithm here. For example:
 #
-#       defaultmd="-sha256"
+#	defaultmd="-sha256"
 #
-defaultmd="-aes256"
+defaultmd="-sha512"
 
 function maxdays() {
-        nowyear=$(date +%Y)
-        years=$(expr 2032 - $nowyear)
-        days=$(expr $years '*' 365)
+	nowyear=$(date +%Y)
+	years=$(expr 2032 - $nowyear)
+	days=$(expr $years '*' 365)
 
-        echo $days
+	echo $days
 }
 
 function getipaddresses() {
-        /sbin/ifconfig |
-                grep -v tunnel |
-                sed -En '/inet6? /p' |
-                sed -Ee 's/inet6? (addr:)?//' |
-                awk '{print $1;}' |
-                sed -e 's/[%/].*//' |
-                egrep -v '(::1|127\.0\.0\.1)'   # omit loopback to add it later
+	/sbin/ifconfig |
+		grep -v tunnel |
+		sed -En '/inet6? /p' |
+		sed -Ee 's/inet6? (addr:)?//' |
+		awk '{print $1;}' |
+		sed -e 's/[%/].*//' |
+		egrep -v '(::1|127\.0\.0\.1)'	# omit loopback to add it later
 }
 
 
 function addresslist() {
 
-        ALIST=""
-        for a in $(getipaddresses); do
-                ALIST="${ALIST}IP:$a,"
-        done
-        ALIST="${ALIST}IP:127.0.0.1,IP:::1,"
+	ALIST=""
+	for a in $(getipaddresses); do
+		ALIST="${ALIST}IP:$a,"
+	done
+	ALIST="${ALIST}IP:127.0.0.1,IP:::1,"
 
-        for ip in $(echo ${ALTADDRESSES}); do
-                ALIST="${ALIST}IP:${ip},"
-        done
-        for h in $(echo ${ALTHOSTNAMES}); do
-                ALIST="${ALIST}DNS:$h,"
-        done
-        ALIST="${ALIST}DNS:localhost"
-        echo $ALIST
+	for ip in $(echo ${ALTADDRESSES}); do
+		ALIST="${ALIST}IP:${ip},"
+	done
+	for h in $(echo ${ALTHOSTNAMES}); do
+		ALIST="${ALIST}DNS:$h,"
+	done
+	ALIST="${ALIST}DNS:${host},DNS:localhost"
+	echo $ALIST
 
 }
 
 days=$(maxdays)
 
+server_days=825	# https://support.apple.com/en-us/HT210176
+
 if [ -n "$CAKILLFILES" ]; then
-        rm -f $CACERT.??? $SERVER.??? $CACERT.srl
+	rm -f $CACERT.??? $SERVER.??? $CACERT.srl
 fi
 
 if [ ! -f $CACERT.crt ]; then
 
-        #    ____    _
-        #   / ___|  / \
-        #  | |     / _ \
-        #  | |___ / ___ \
-        #   \____/_/   \_\
-        #
+	#    ____    _    
+	#   / ___|  / \   
+	#  | |     / _ \  
+	#  | |___ / ___ \ 
+	#   \____/_/   \_\
+	#                 
 
-        # Create un-encrypted (!) key
-        $openssl req -newkey rsa:${keybits} -x509 -nodes $defaultmd -days $days -extensions v3_ca -keyout $CACERT.key -out $CACERT.crt -subj "${CA_DN}"
-        echo "Created CA certificate in $CACERT.crt"
-        $openssl x509 -in $CACERT.crt -nameopt multiline -subject -noout
+	# Create un-encrypted (!) key
+	$openssl req -newkey rsa:${keybits} -x509 -nodes $defaultmd -days $days -extensions v3_ca -keyout $CACERT.key -out $CACERT.crt -subj "${CA_DN}"
+	echo "Created CA certificate in $CACERT.crt"
+	$openssl x509 -in $CACERT.crt -nameopt multiline -subject -noout
 
-        chmod 400 $CACERT.key
-        chmod 444 $CACERT.crt
-        chown $MOSQUITTOUSER $CACERT.*
-        echo "Warning: the CA key is not encrypted; store it safely!"
+	chmod 400 $CACERT.key
+	chmod 444 $CACERT.crt
+	chown $MOSQUITTOUSER $CACERT.*
+	echo "Warning: the CA key is not encrypted; store it safely!"
 fi
 
 
 if [ $kind == 'server' ]; then
 
-        #   ____
-        #  / ___|  ___ _ ____   _____ _ __
-        #  \___ \ / _ \ '__\ \ / / _ \ '__|
-        #   ___) |  __/ |   \ V /  __/ |
-        #  |____/ \___|_|    \_/ \___|_|
-        #
+	#   ____                           
+	#  / ___|  ___ _ ____   _____ _ __ 
+	#  \___ \ / _ \ '__\ \ / / _ \ '__|
+	#   ___) |  __/ |   \ V /  __/ |   
+	#  |____/ \___|_|    \_/ \___|_|   
+	#                                  
 
-        if [ ! -f $SERVER.key ]; then
-                echo "--- Creating server key and signing request"
-                $openssl genrsa -out $SERVER.key $keybits
-                $openssl req -new $defaultmd \
-                        -out $SERVER.csr \
-                        -key $SERVER.key \
-                        -subj "${SERVER_DN}"
-                chmod 400 $SERVER.key
-                chown $MOSQUITTOUSER $SERVER.key
-        fi
+	if [ ! -f $SERVER.key ]; then
+		echo "--- Creating server key and signing request"
+		$openssl genrsa -out $SERVER.key $keybits
+		$openssl req -new $defaultmd \
+			-out $SERVER.csr \
+			-key $SERVER.key \
+			-subj "${SERVER_DN}"
+		chmod 400 $SERVER.key
+		chown $MOSQUITTOUSER $SERVER.key
+	fi
 
-        if [ -f $SERVER.csr -a ! -f $SERVER.crt ]; then
+	if [ -f $SERVER.csr -a ! -f $SERVER.crt ]; then
 
-                # There's no way to pass subjAltName on the CLI so
-                # create a cnf file and use that.
+		# There's no way to pass subjAltName on the CLI so
+		# create a cnf file and use that.
 
-                CNF=`mktemp /tmp/cacnf.XXXXXXXX` || { echo "$0: can't create temp file" >&2; exit 1; }
-                sed -e 's/^.*%%% //' > $CNF <<\!ENDconfig
-                %%% [ JPMextensions ]
-                %%% basicConstraints        = critical,CA:false
-                %%% nsCertType              = server
-                %%% keyUsage                = nonRepudiation, digitalSignature, keyEncipherment
-                %%% nsComment               = "Broker Certificate"
-                %%% subjectKeyIdentifier    = hash
-                %%% authorityKeyIdentifier  = keyid,issuer:always
-                %%% subjectAltName          = $ENV::SUBJALTNAME
-                %%% # issuerAltName           = issuer:copy
-                %%% ## nsCaRevocationUrl       = http://mqtt.org/carev/
-                %%% ## nsRevocationUrl         = http://mqtt.org/carev/
-                %%% certificatePolicies     = ia5org,@polsection
-                %%% [polsection]
-                %%% policyIdentifier        = 1.3.5.8
-                %%% CPS.1                   = "http://localhost"
-                %%% userNotice.1            = @notice
-                %%% [notice]
-                %%% explicitText            = "This CA is for a local MQTT broker installation only"
-                %%% organization            = "IoT"
-                %%% noticeNumbers           = 1
+		CNF=`mktemp /tmp/cacnf.XXXXXXXX` || { echo "$0: can't create temp file" >&2; exit 1; }
+		sed -e 's/^.*%%% //' > $CNF <<\!ENDconfig
+		%%% [ JPMextensions ]
+		%%% basicConstraints        = critical,CA:false
+		%%% nsCertType              = server
+		%%% keyUsage                = nonRepudiation, digitalSignature, keyEncipherment
+		%%% extendedKeyUsage        = serverAuth
+		%%% nsComment               = "Broker Certificate"
+		%%% subjectKeyIdentifier    = hash
+		%%% authorityKeyIdentifier  = keyid,issuer:always
+		%%% subjectAltName          = $ENV::SUBJALTNAME
+		%%% # issuerAltName           = issuer:copy
+		%%% ## nsCaRevocationUrl       = http://mqttitude.org/carev/
+		%%% ## nsRevocationUrl         = http://mqttitude.org/carev/
+		%%% certificatePolicies     = ia5org,@polsection
+		%%% 
+		%%% [polsection]
+		%%% policyIdentifier	    = 1.3.5.8
+		%%% CPS.1		    = "http://localhost"
+		%%% userNotice.1	    = @notice
+		%%% 
+		%%% [notice]
+		%%% explicitText            = "This CA is for a local MQTT broker installation only"
+		%%% organization            = "AXMOS Technologies"
+		%%% noticeNumbers           = 1
 
 !ENDconfig
 
-                SUBJALTNAME="$(addresslist)"
-                export SUBJALTNAME              # Use environment. Because I can. ;-)
+		SUBJALTNAME="$(addresslist)"
+		export SUBJALTNAME		# Use environment. Because I can. ;-)
 
-                echo "--- Creating and signing server certificate"
-                $openssl x509 -req $defaultmd \
-                        -in $SERVER.csr \
-                        -CA $CACERT.crt \
-                        -CAkey $CACERT.key \
-                        -CAcreateserial \
-                        -CAserial "${DIR}/ca.srl" \
-                        -out $SERVER.crt \
-                        -days $days \
-                        -extfile ${CNF} \
-                        -extensions JPMextensions
+		echo "--- Creating and signing server certificate"
+		$openssl x509 -req $defaultmd \
+			-in $SERVER.csr \
+			-CA $CACERT.crt \
+			-CAkey $CACERT.key \
+			-CAcreateserial \
+			-CAserial "${DIR}/ca.srl" \
+			-out $SERVER.crt \
+			-days $server_days \
+			-extfile ${CNF} \
+			-extensions JPMextensions
 
-                rm -f $CNF
-                chmod 444 $SERVER.crt
-                chown $MOSQUITTOUSER $SERVER.crt
-        fi
+		rm -f $CNF
+		chmod 444 $SERVER.crt
+		chown $MOSQUITTOUSER $SERVER.crt
+	fi
 else
-        #    ____ _ _            _
-        #   / ___| (_) ___ _ __ | |_
-        #  | |   | | |/ _ \ '_ \| __|
-        #  | |___| | |  __/ | | | |_
-        #   \____|_|_|\___|_| |_|\__|
-        #
+	#    ____ _ _            _   
+	#   / ___| (_) ___ _ __ | |_ 
+	#  | |   | | |/ _ \ '_ \| __|
+	#  | |___| | |  __/ | | | |_ 
+	#   \____|_|_|\___|_| |_|\__|
+	#                            
 
-        if [ ! -f $CLIENT.key ]; then
-                echo "--- Creating client key and signing request"
-                $openssl genrsa -out $CLIENT.key $keybits
+	if [ ! -f $CLIENT.key ]; then
+		echo "--- Creating client key and signing request"
+		$openssl genrsa -out $CLIENT.key $keybits
 
-                CNF=`mktemp /tmp/cacnf-req.XXXXXXXX` || { echo "$0: can't create temp file" >&2; exit 1; }
-                # Mosquitto's use_identity_as_username takes the CN attribute
-                # so we're populating that with the client's name
-                sed -e 's/^.*%%% //' > $CNF <<!ENDClientconfigREQ
-                %%% [ req ]
-                %%% distinguished_name  = req_distinguished_name
-                %%% prompt                      = no
-                %%% output_password             = secret
-                %%% [ req_distinguished_name ]
-                %%% # O                       = OwnTracks
-                %%% # OU                      = MQTT
-                %%% # CN                      = Suzie Smith
-                %%% CN                        = $CLIENT
-                %%% # emailAddress            = $CLIENT
+		CNF=`mktemp /tmp/cacnf-req.XXXXXXXX` || { echo "$0: can't create temp file" >&2; exit 1; }
+		# Mosquitto's use_identity_as_username takes the CN attribute
+		# so we're populating that with the client's name
+		sed -e 's/^.*%%% //' > $CNF <<!ENDClientconfigREQ
+		%%% [ req ]
+		%%% distinguished_name	= req_distinguished_name
+		%%% prompt			= no
+		%%% output_password		= secret
+		%%% 
+		%%% [ req_distinguished_name ]
+		%%% # O                       = OwnTracks
+		%%% # OU                      = MQTT
+		%%% # CN                      = Suzie Smith
+		%%% CN                        = $CLIENT
+		%%% # emailAddress            = $CLIENT
 !ENDClientconfigREQ
 
-                $openssl req -new $defaultmd \
-                        -out $CLIENT.csr \
-                        -key $CLIENT.key \
-                        -config $CNF
-                chmod 400 $CLIENT.key
-        fi
+		$openssl req -new $defaultmd \
+			-out $CLIENT.csr \
+			-key $CLIENT.key \
+			-config $CNF
+		chmod 400 $CLIENT.key
+	fi
 
-        if [ -f $CLIENT.csr -a ! -f $CLIENT.crt ]; then
+	if [ -f $CLIENT.csr -a ! -f $CLIENT.crt ]; then
 
-                CNF=`mktemp /tmp/cacnf-cli.XXXXXXXX` || { echo "$0: can't create temp file" >&2; exit 1; }
-                sed -e 's/^.*%%% //' > $CNF <<\!ENDClientconfig
-                %%% [ JPMclientextensions ]
-                %%% basicConstraints        = critical,CA:false
-                %%% subjectAltName          = email:copy
-                %%% nsCertType              = client,email
-                %%% extendedKeyUsage        = clientAuth,emailProtection
-                %%% keyUsage                = digitalSignature, keyEncipherment, keyAgreement
-                %%% nsComment               = "Client Broker Certificate"
-                %%% subjectKeyIdentifier    = hash
-                %%% authorityKeyIdentifier  = keyid,issuer:always
+		CNF=`mktemp /tmp/cacnf-cli.XXXXXXXX` || { echo "$0: can't create temp file" >&2; exit 1; }
+		sed -e 's/^.*%%% //' > $CNF <<\!ENDClientconfig
+		%%% [ JPMclientextensions ]
+		%%% basicConstraints        = critical,CA:false
+		%%% subjectAltName          = email:copy
+		%%% nsCertType              = client,email
+		%%% extendedKeyUsage        = clientAuth,emailProtection
+		%%% keyUsage                = digitalSignature, keyEncipherment, keyAgreement
+		%%% nsComment               = "Client Broker Certificate"
+		%%% subjectKeyIdentifier    = hash
+		%%% authorityKeyIdentifier  = keyid,issuer:always
 
 !ENDClientconfig
 
-                SUBJALTNAME="$(addresslist)"
-                export SUBJALTNAME              # Use environment. Because I can. ;-)
+		SUBJALTNAME="$(addresslist)"
+		export SUBJALTNAME		# Use environment. Because I can. ;-)
 
-                echo "--- Creating and signing client certificate"
-                $openssl x509 -req $defaultmd \
-                        -in $CLIENT.csr \
-                        -CA $CACERT.crt \
-                        -CAkey $CACERT.key \
-                        -CAcreateserial \
-                        -CAserial "${DIR}/ca.srl" \
-                        -out $CLIENT.crt \
-                        -days $days \
-                        -extfile ${CNF} \
-                        -extensions JPMclientextensions
+		echo "--- Creating and signing client certificate"
+		$openssl x509 -req $defaultmd \
+			-in $CLIENT.csr \
+			-CA $CACERT.crt \
+			-CAkey $CACERT.key \
+			-CAcreateserial \
+			-CAserial "${DIR}/ca.srl" \
+			-out $CLIENT.crt \
+			-days $days \
+			-extfile ${CNF} \
+			-extensions JPMclientextensions
 
-                rm -f $CNF
-                chmod 444 $CLIENT.crt
-        fi
+		rm -f $CNF
+		chmod 444 $CLIENT.crt
+	fi
 fi
